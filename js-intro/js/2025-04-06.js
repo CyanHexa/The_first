d8b5fc37446f6e544b2140b1dfa7a50cb5d13f99
Cyan Hexagonovich Bezdarev <you@example.com>
2025-04-13 15:29:58 +0300

    poka kalkulator

--- js-intro/js/2025-04-06.js
+++ js-intro/js/2025-04-06.js
@@ -1,32 +1,48 @@
-let num1, num2, sign, result = NaN;
+function calculate(sign, num1, num2) {
 
-num1 = Number(prompt("введите первое число", "0"));
-num2 = Number(prompt("введите второе число", "0"));
-sign = prompt("введите действие (+ - * /)", "+");
+    let result = NaN;
 
-switch (sign) {
-    case "+":
-        result = num1 + num2;
-        break;
-
-    case "-":
-        result = num1 - num2;
-        break;
-
-    case "*":
-        result = num1 * num2;
-        break;
-    case '/':
-        if (num2 === 0) {
-            alert("Деление на ноль!");
-        } else {
-            result = num1 / num2;
-        }
-        break;
+    switch (sign) {
+        case "+":
+            result = num1 + num2;
+            break;
+    
+        case "-":
+            result = num1 - num2;
+            break;
+    
+        case "*":
+            result = num1 * num2;
+            break;
+        case '/':
+            if (num2 === 0) {
+                alert("Деление на ноль!");
+            } else {
+                result = num1 / num2;
+            }
+            break;
+    }
+    return result;
 }
+
+function calculator() {
+    let num1, num2, sign, result = NaN;
+    if (!confirm("Запустьт калькуклятор?")) return false;
+    num1 = Number(prompt("введите первое число", "0"));
+    num2 = Number(prompt("введите второе число", "0"));
+    sign = prompt("введите действие (+ - * /)", "+");
+
+result = calculate(sign, num1, num2)
 
 if (isNaN(result)) {
     alert("Некорректный ввод!");
 } else {
     alert(`Ответ: ${result}`);
 }
+
+    return true
+};
+
+
+let needRun = true
+while (calculator());
